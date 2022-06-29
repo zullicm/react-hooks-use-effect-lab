4c303ef4a484eab05bfa8b915ec0de9088e53169
Christopher Marzulli <chriszulli15@gmail.com>
2022-06-29 13:08:52 -0400

    commit tree feaf1aa6792259329f42ee5e3dd9bbbb878b7b5e

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(timeRemaining > 0){
+        return setTimeRemaining(() => timeRemaining - 1)
+      } else if(timeRemaining === 0){
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
